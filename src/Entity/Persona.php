e1f43e733e5e62ac29589ffc84ddf506751ea2b8
belliniedu <belliniedu@gmail.com>
2021-06-25 18:50:30 -0300

    pasado a symfony 5

---
+++ src/Entity/Persona.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonaRepository::class)
+ */
+class Persona
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellido;
+
+    /**
+     * @ORM\Column(type="string", length=12)
+     */
+    private $dni;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $fechaNacimiento;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Tarea::class, inversedBy="personas")
+     */
+    private $tareas;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, inversedBy="persona", cascade={"persist", "remove"})
+     */
+    private $User;
+
+    public function __construct()
+    {
+        $this->tareas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    public function getDni(): ?string
+    {
+        return $this->dni;
+    }
+
+    public function setDni(string $dni): self
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fechaNacimiento;
+    }
+
+    public function setFechaNacimiento(?\DateTimeInterface $fechaNacimiento): self
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tarea[]
+     */
+    public function getTareas(): Collection
+    {
+        return $this->tareas;
+    }
+
+    public function addTarea(Tarea $tarea): self
+    {
+        if (!$this->tareas->contains($tarea)) {
+            $this->tareas[] = $tarea;
+        }
+
+        return $this;
+    }
+
+    public function removeTarea(Tarea $tarea): self
+    {
+        $this->tareas->removeElement($tarea);
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): self
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Tarea.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TareaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TareaRepository::class)
+ */
+class Tarea
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $estado;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $fechaLimite;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Persona::class, mappedBy="tareas")
+     */
+    private $personas;
+
+    public function __construct()
+    {
+        $this->personas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(?string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getEstado(): ?string
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(string $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    public function getFechaLimite(): ?\DateTimeInterface
+    {
+        return $this->fechaLimite;
+    }
+
+    public function setFechaLimite(?\DateTimeInterface $fechaLimite): self
+    {
+        $this->fechaLimite = $fechaLimite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Persona[]
+     */
+    public function getPersonas(): Collection
+    {
+        return $this->personas;
+    }
+
+    public function addPersona(Persona $persona): self
+    {
+        if (!$this->personas->contains($persona)) {
+            $this->personas[] = $persona;
+            $persona->addTarea($this);
+        }
+
+        return $this;
+    }
+
+    public function removePersona(Persona $persona): self
+    {
+        if ($this->personas->removeElement($persona)) {
+            $persona->removeTarea($this);
+        }
+
+        return $this;
+    }
+}
